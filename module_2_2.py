bb81dec9c63a1d3e3e050329c301e8542000b392
Igor8mmk <igor.mmk1@gmail.com>
2024-08-06 15:06:00 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = 10
+second = 18
+third = 17
+if first==second==third:
+    print('Одинаковых числел:', 3)
+elif first==second or second==third or first==third:
+    print('Одинаковых числел:', 2)
+else:
+    print('Одинаковых чисел:', 0)
